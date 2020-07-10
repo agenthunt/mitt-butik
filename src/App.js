d74d8d21831b32da08639b8078f0074ca5d3b1d4
Shailesh Kumar <shailesh.kumar@outlook.com>
2020-07-10 15:18:42 +0200

    Built, Deployed and Hosted using Netlify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
@@ -10,14 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mitt Butik
-        </a>
+        <p>Mitt Butik</p>
+        <p> Build, Deployed and Hosted using Netlify </p>
       </header>
     </div>
   );
